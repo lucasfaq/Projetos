cf2a53b1dc923591c34d113de3aca8dc377566c7
Lucas Frota de Aquino <lucasft@gmail.com>
2022-04-27 11:40:05 -0300

    Foram removidas as linhas comentadas

--- gitupdate.py
+++ gitupdate.py
@@ -4,9 +4,6 @@ from git import Repo, GitCommandError
 paths = getcwd()
 m_repo = Repo(paths)
 
-#for remote in m_repo.remotes:
-    #print(f'- {remote.name} {remote.url}')
-
 def git_push():
     try:
         m_repo.git.add(A=True)
